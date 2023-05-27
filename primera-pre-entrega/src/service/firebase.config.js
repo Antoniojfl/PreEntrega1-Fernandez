6ee53bacf5a769524cdddbf80249d8b6308336b8
antonio <antonioptc23@gmail.com>
2023-05-26 22:23:43 -0400

    minor bugsfixed 2

--- primera-pre-entrega/src/service/firebase.config.js
+++ primera-pre-entrega/src/service/firebase.config.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_REACT_APP_appId
 };
 
-console.log('firebaseConfig: ', firebaseConfig)
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
